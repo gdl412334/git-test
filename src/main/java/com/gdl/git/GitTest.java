01aa363fc8ceaf13aec39350678c371bd2bd42a1
gaodelong <412334164@qq.com>
2021-12-01 13:46:42 +0800

    first commit

---
+++ src/main/java/com/gdl/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.gdl.git;
+
+/**
+ * @Description
+ * @Author 高德龙
+ * @Date 2021/12/1 13:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
